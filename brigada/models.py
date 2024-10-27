00c8b10618e2d26840c4eed1e9f9cd2c07abe3b8
Nicolascard995 <nicocard995@gmail.com>
2024-10-27 11:38:16 +0100

    Sprint1_01

--- brigada/models.py
+++ brigada/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50)  # Ej. 'Chef', 'Sous Chef', 'Cocinero'
+    schedule = models.CharField(max_length=100)  # Ej. "Lunes a Viernes, 9am-5pm"
+
+    def __str__(self):
+        return f'{self.user.username} - {self.role}'
--- brigada/views.py
+++ brigada/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Member
+from .serializers import MemberSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+    permission_classes = [IsAuthenticated]  # Asegura que solo usuarios autenticados puedan acceder
